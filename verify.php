1cec3407fda4728b535667aa4d92ba5517be63af
mayaswan <33310757+mayaswan@users.noreply.github.com>
2018-09-22 00:52:47 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'RVPWj6aEOnRaw6bo/n8u9+W2Sxb7VQlCJ32AzvkqdHjSbPEIR+6I6btcmbozz4Y8eVx6J2n0guQ4TFAVlf8Ihwudfs3Zw/5SbM81x0NrvRk5IXYpzHFhMkWBtXGZdKqqSV+JSH61mNvU+P89RkraLAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
