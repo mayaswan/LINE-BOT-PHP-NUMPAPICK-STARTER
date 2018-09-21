442b02926cb0e9866c0bd0bfbe572ada9d277f00
mayaswan <33310757+mayaswan@users.noreply.github.com>
2018-09-21 21:09:58 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '8Hy46nH62hERg3povhPe70MuuAP0KCkxxk+Os2x52XoMcJxeBZrfyKDUBt/4m9M0eVx6J2n0guQ4TFAVlf8Ihwudfs3Zw/5SbM81x0NrvRlsE/UvQ/TzLcpno5153qtOVOzFCgQa0tJw+BWaMkJuZgdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U71734c3d0e820238458c6a5c8c644ca8',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
