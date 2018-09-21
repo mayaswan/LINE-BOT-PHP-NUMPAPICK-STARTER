17dea605fbf4288e40dd48418ce59be959e3d7ca
mayaswan <33310757+mayaswan@users.noreply.github.com>
2018-09-22 00:33:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'ON' && $event['message']['type'] == 'ON') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
