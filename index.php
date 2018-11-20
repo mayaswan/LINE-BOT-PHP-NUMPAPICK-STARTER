44fff2861f9f9760c426b091467c6aa3ef9945db
mayaswan <33310757+mayaswan@users.noreply.github.com>
2018-11-20 18:09:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">@BaaNPark</a>
+      <a class="navbar-brand" href="index.php">@BaaNPark</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
+      <li class="active"><a href="index.php">Home</a></li>
       <li><a href="#">Page 1</a></li>
       <li><a href="#">Page 2</a></li>
     </ul>
