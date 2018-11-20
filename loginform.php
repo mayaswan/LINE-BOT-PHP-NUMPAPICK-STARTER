47f8958797dc788c5a30db1d24f2236e0fa78c75
mayaswan <33310757+mayaswan@users.noreply.github.com>
2018-11-20 18:05:55 +0700

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -9,6 +9,36 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">@BaaNPark</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+    </ul>
+    
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="loginform.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+    
+    <form class="navbar-form navbar-right" action="/action_page.php">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search" name="search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+          
+          
+        </div>
+        
+      </div>
+    </form>
+    
+  </div>
+</nav>
+<center>
 
 <div class="container">
   <h2>Login to Account</h2>
@@ -17,8 +47,9 @@
  
       <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"><br><br>
 
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd"><br><br>
       <button type="submit" class="btn btn-default">Login</button>
+      <button type="submit" class="btn btn-default">Cancle</button>
     </div>
     
   </form>
