ea07666a2e58b6f6dcf9131c9383a480859f3914
mayaswan <33310757+mayaswan@users.noreply.github.com>
2019-03-18 16:53:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,60 +2,29 @@
 <html lang="en">
 <head>
   <title>@BaanPakr</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta charset="utf-8"> 
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">@BaaNPark</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php">หน้าหลัก </a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">บริการ <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#"> - อาหาร</a></li>
-          <li><a href="#"> - พักผ่อน</a></li>
-          <li><a href="#"> - การท่องเที่ยว</a></li>
-          
-        </ul>
-      </li>
-      <li><a href="Location.php">สถานที่</a></li>
-    </ul>
-    
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="loginform.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-    
-    <form class="navbar-form navbar-right" action="/action_page.php">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" name="search">
-        <div class="input-group-btn">
-          <button class="btn btn-default" type="submit">
-            <i class="glyphicon glyphicon-search"></i>
-          </button>
-          
-          
-        </div>
-        
-      </div>
-    </form>
-    
-  </div>
-</nav>
+<h1 id="bigOne"></h1>
 
-<div class="container">
-  <h3>เกี่ยวกับเรา</h3>
-<!--<p>สวัสดีคัรบทุกท่าน เรายินดีนำเสนอ สถานที่ท่องเที่ยวสุดบรรยากาศ</p> -->
-  <p>ปาร์ครักแอ็ค นะคัรบ</p>
-</div>
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBQPtYMWRKqohvVp3Rx_utju2qotiYJxjQ",
+    authDomain: "webapp-bf8f5.firebaseapp.com",
+    databaseURL: "https://webapp-bf8f5.firebaseio.com",
+    projectId: "webapp-bf8f5",
+    storageBucket: "webapp-bf8f5.appspot.com",
+    messagingSenderId: "995463764163"
+  };
+  firebase.initializeApp(config);
+  var bigOne = document.getElemrntById('bigOne');
+  var dbRef = firebase.database().ref().child('text');
+  dbRef.on('value',snap => bigOne.innerText = snap.val());
+</script>
+
 
 </body>
 </html>
-
